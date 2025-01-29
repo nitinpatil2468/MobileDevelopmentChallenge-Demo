b2900d06051d0fe491bd4adbaad28249c83b821c
Dipesh Bharti <dbharti@ccep.com>
2025-01-29 11:01:01 +0530

    Initial Commit

---
+++ MobileDevelopmentChallenge/MobileDevelopmentChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevelopmentChallengeApp.swift
+//  MobileDevelopmentChallenge
+//
+//  Created by capgemini on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevelopmentChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
